67ddae5701fd42bd382ed6315e3100c26d4a4712
balajiNadar08 <balajigittttt@gmail.com>
2026-07-04 22:56:45 +0530

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <h1>Linkedin blocker</h1>;
+  return (
+    <div style={{ padding: "20px", width: "250px" }}>
+      <h2>LinkedIn Blocker</h2>
+
+      <button>Enable</button>
+    </div>
+  );
 }
 
 export default App;
