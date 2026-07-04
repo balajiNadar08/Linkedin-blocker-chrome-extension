fa3865db6db006af46677abcddc3f11855605ae7
balajiNadar08 <balajigittttt@gmail.com>
2026-07-04 22:56:07 +0530

    Manifest file for extension

---
+++ manifest.config.js
@@ -0,0 +1,33 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export default defineManifest({
+  manifest_version: 3,
+
+  name: "LinkedIn Feed Blocker",
+
+  version: "1.0.0",
+
+  description: "Hide LinkedIn posts based on your preferences.",
+
+  permissions: ["storage"],
+
+  host_permissions: ["https://www.linkedin.com/*"],
+
+  action: {
+    default_popup: "index.html",
+    default_title: "LinkedIn Feed Blocker",
+  },
+
+  content_scripts: [
+    {
+      matches: ["https://www.linkedin.com/*"],
+      js: ["src/content.js"],
+    },
+  ],
+
+  // icons: {
+  //   16: "icon16.png",
+  //   48: "icon48.png",
+  //   128: "icon128.png",
+  // },
+});
