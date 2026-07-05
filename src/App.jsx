c3b38da06d2a5343c684d07f77af0d691940f938
balajiNadar08 <balajigittttt@gmail.com>
2026-07-05 16:41:19 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 
 function App() {
+
   return (
-    <div style={{ padding: "20px", width: "250px" }}>
+    <div
+      style={{
+        padding: "20px",
+        width: "250px",
+        backgroundColor: "red",
+        color: "white",
+      }}
+    >
       <h2>LinkedIn Blocker</h2>
 
-      <button>Enable</button>
+      <button style={{ cursor: "pointer" }}>
+        Enable
+      </button>
     </div>
   );
 }
